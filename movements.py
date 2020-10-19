35fa18143d368aa131d46ac6546674adb187fbd0
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-10-19 15:59:02 -0500

    Better histos

--- movements.py
+++ movements.py
@@ -3,6 +3,7 @@ from math import fabs
 values = []
 prev = None
 pip = 0.0001 # EUR-USD
+span = 100
 with open('demo.dat') as data:
     for line in data:
         data = line.split()
@@ -15,22 +16,46 @@ with open('demo.dat') as data:
                     values.append(change)
             prev = curr
 
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.stats as stats
 
-plt.xlabel('Change in PIPs in closing price')
-plt.xlim((-100, 100))
-plt.ylabel('Relative frequency')
-y, x, p = plt.hist(values, density = True, bins = 30)
+import numpy as np
+import seaborn as sns
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+from statsmodels.graphics.gofplots import qqplot
+
+bins = [32, 64, 128]
+threshold = 0.01
 mu = np.mean(values) 
 sigma = np.std(values)
-plt.plot(x, stats.norm.pdf(x, mu, sigma))
-plt.savefig('relative.png', dip = 150)
-print(mu, sigma)
-plt.clf()
+fig, axs = plt.subplots(3, len(bins),  tight_layout = True, figsize = (12, 9))
+for i in range(len(bins)):
+    ax = axs[0, i]
+    sns.distplot(values, hist = True, kde = True, bins = bins[i], color = 'gray',  hist_kws = {'edgecolor' : 'black'}, kde_kws = {'linewidth': 1, 'color': 'blue'}, ax = ax)
+    ax.set_xlim((-1.1 * span, 1.1 * span))
+    ax.set_ylim((0, 0.05))
+    ax.set_ylabel('Density')
+    ax.set_xlabel(f'Change in PIP ({bins[i]} bins)')
+    x = np.linspace(-span, span, 100)
+    ax.plot(x, stats.norm.pdf(x, mu, sigma), color = 'red')    
+    qqplot(np.array(values), line = 's', ax = axs[1, i], marker = '.', color = 'gray')
+    h = stats.relfreq(values, numbins = bins[i])
+    xp = h.lowerlimit + np.linspace(0, h.binsize * h.frequency.size, h.frequency.size)
+    ax = axs[2, i]
+    y = []
+    x = []
+    j = 0
+    for j in range(len(xp)):
+        v = h.frequency[j]
+        if v >= threshold:
+            y.append(v)
+            x.append(xp[j])
+        j += i
+    ax.set_xlim((-1.1 * span, 1.1 * span))
+    ax.set_ylim((0, 0.7))
+    ax.set_ylabel('Relative frequency')
+    ax.set_xlabel(f'Change in PIP ({len(x)} significant bins)')        
+    ax.bar(x, y, width = h.binsize)
+    plt.savefig('movements.png', dpi = 150)
+        
 
 
-from statsmodels.graphics.gofplots import qqplot
-qqplot(np.array(values), line='s')
-plt.savefig('qq.png')
